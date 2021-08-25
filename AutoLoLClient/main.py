1a9b4ea2c020d0fc3430c427498c0db2425889eb
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 23:57:22 +0200

    Wrote simple gui to display the status of the lol client

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -1,8 +1,29 @@
+import time
+
 from AutoLoLClient.LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
 
-if __name__ == '__main__':
-    lol_client_window = LeagueOfLegendsClientWindow()
-    print(f"Pos: {lol_client_window.get_pos()}")
-    print(f"Size: {lol_client_window.get_size()}")
+import PySimpleGUI
+import tkinter as tk
 
-    print(f"In Queue: {lol_client_window.in_queue()}")
+
+class StatusWindow(tk.Frame):
+    def __init__(self, parent):
+        super(StatusWindow, self).__init__(parent)
+
+        self.lol_client_window = LeagueOfLegendsClientWindow()
+
+        self.pos_label = tk.Label(self, text=f"Window Pos: {self.lol_client_window.get_pos()}")
+        self.pos_label.pack(padx=20, pady=20)
+
+        self.size_label = tk.Label(self, text=f"Window Size: {self.lol_client_window.get_size()}")
+        self.size_label.pack(padx=20, pady=20)
+
+        self.queue_label = tk.Label(self, text=f"Window Size: {self.lol_client_window.in_queue()}")
+        self.queue_label.pack(padx=20, pady=20)
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    statusWindow = StatusWindow(root)
+    statusWindow.pack(fill="both", expand=True)
+    root.mainloop()
