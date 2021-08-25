d2f809183f63b62a66e5e39e66a0383b374f1d94
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 15:57:32 +0200

    Implemented is_open to prevent error if lol client was closed while running

--- LeagueOfLegendsClientWindow.py
+++ LeagueOfLegendsClientWindow.py
@@ -3,29 +3,36 @@ from typing import Tuple
 from win32gui import FindWindow, GetWindowRect
 
 
-class LoLClientWindow(object):
+class LeagueOfLegendsClientWindow(object):
     def __init__(self, window_name: str = "League of Legends"):
         self.window_name = window_name
 
+        self.league_of_legends_window = FindWindow(None, self.window_name)  # TODO exception
+
+    def is_open(self):
         self.league_of_legends_window = FindWindow(None, self.window_name)
-        if self.league_of_legends_window == 0:
-            print("ERROR: League of Legends window not found")  # TODO exception
-            print("INFO: Please issue your League of Legends Client window name to GitHub")
-            exit(1)
+        window_is_open = self.league_of_legends_window != 0
+        return window_is_open
 
     def get_pos(self) -> Tuple[int, int]:
+        if not self.is_open():
+            return -1, -1  # TODO exception
+
         league_of_legends_window_rect = GetWindowRect(self.league_of_legends_window)
         league_of_legends_window_x = league_of_legends_window_rect[0]
         league_of_legends_window_y = league_of_legends_window_rect[2]
         return league_of_legends_window_x, league_of_legends_window_y
 
     def get_size(self) -> Tuple[int, int]:
+        if not self.is_open():
+            return -1, -1  # TODO exception
+
         league_of_legends_window_rect = GetWindowRect(self.league_of_legends_window)
 
         league_of_legends_window_x = league_of_legends_window_rect[0]
-        league_of_legends_window_x_end = league_of_legends_window_rect[1]
+        league_of_legends_window_x_end = league_of_legends_window_rect[2]
 
-        league_of_legends_window_y = league_of_legends_window_rect[2]
+        league_of_legends_window_y = league_of_legends_window_rect[1]
         league_of_legends_window_y_end = league_of_legends_window_rect[3]
 
         league_of_legends_window_width = league_of_legends_window_x_end - league_of_legends_window_x
