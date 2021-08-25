8c49d057112141761ee538b32f195a1f7c1fc7b6
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 15:40:05 +0200

    Implemented get_pos

--- LoLClientWindow.py
+++ LoLClientWindow.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from win32gui import FindWindow, GetWindowRect
 
 
@@ -5,8 +7,17 @@ class LoLClientWindow(object):
     def __init__(self, window_name: str = "League of Legends"):
         self.window_name = window_name
 
-    def get_pos(self):
-        pass
+        self.league_of_legends_window = FindWindow(None, self.window_name)
+        if self.league_of_legends_window == 0:
+            print("ERROR: League of Legends window not found")  # TODO exception
+            print("INFO: Please issue your League of Legends Client window name to GitHub")
+            exit(1)
+
+    def get_pos(self) -> Tuple[int, int]:
+        league_of_legends_window_rect = GetWindowRect(self.league_of_legends_window)
+        league_of_legends_window_x = league_of_legends_window_rect[0]
+        league_of_legends_window_y = league_of_legends_window_rect[2]
+        return league_of_legends_window_x, league_of_legends_window_y
 
     def get_size(self):
         pass
