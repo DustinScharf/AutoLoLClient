c42add64ce3c9a8875cc8ef849014930320cda93
DustinScharf <d.scharf.2002@gmail.com>
2021-08-26 00:32:54 +0200

    Added 1 second wait between gui updates

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -3,13 +3,14 @@ import time
 
 from AutoLoLClient.LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
 
-import PySimpleGUI
 import tkinter as tk
 
 
 class StatusWindow(tk.Frame):
     def __init__(self, parent):
         super(StatusWindow, self).__init__(parent)
+
+        self.is_running = True
 
         self.lol_client_window = LeagueOfLegendsClientWindow()
 
@@ -32,12 +33,13 @@ class StatusWindow(tk.Frame):
         update_thread.start()
 
     def update(self):
-        while True:
+        while self.is_running:
             self.pos_label.config(text=f"Window Pos: {self.lol_client_window.get_pos()}")
             self.size_label.config(text=f"Window Size: {self.lol_client_window.get_size()}")
             self.queue_label.config(text=f"In Queue: {self.lol_client_window.in_queue()}")
             self.found_game_label.config(text=f"Found Game: {'TODO'}")
             self.in_game_label.config(text=f"In Game: {'TODO'}")
+            time.sleep(1)
 
 
 if __name__ == '__main__':
