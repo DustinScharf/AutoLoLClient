ed2da58effa80502e43a73a805ac4b379c27ec87
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 15:58:01 +0200

    Created basic test for finding the lol client window

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
+
 if __name__ == '__main__':
-    print('Hello World')
+    lol_client_window = LeagueOfLegendsClientWindow()
+    print(f"Pos: {lol_client_window.get_pos()}")
+    print(f"Size: {lol_client_window.get_size()}")
