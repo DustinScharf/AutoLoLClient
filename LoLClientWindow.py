d4254334df4aeffea5dc041632b11ed6b5941101
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 15:43:02 +0200

    Implemented get_size

--- LoLClientWindow.py
+++ LoLClientWindow.py
@@ -19,5 +19,16 @@ class LoLClientWindow(object):
         league_of_legends_window_y = league_of_legends_window_rect[2]
         return league_of_legends_window_x, league_of_legends_window_y
 
-    def get_size(self):
-        pass
+    def get_size(self) -> Tuple[int, int]:
+        league_of_legends_window_rect = GetWindowRect(self.league_of_legends_window)
+
+        league_of_legends_window_x = league_of_legends_window_rect[0]
+        league_of_legends_window_x_end = league_of_legends_window_rect[1]
+
+        league_of_legends_window_y = league_of_legends_window_rect[2]
+        league_of_legends_window_y_end = league_of_legends_window_rect[3]
+
+        league_of_legends_window_width = league_of_legends_window_x_end - league_of_legends_window_x
+        league_of_legends_window_height = league_of_legends_window_y_end - league_of_legends_window_y
+
+        return league_of_legends_window_width, league_of_legends_window_height
