3db8d6de6ab4cf606d3d28b4c356190f096b0276
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 17:28:14 +0200

    Added test for in_queue

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -4,3 +4,5 @@ if __name__ == '__main__':
     lol_client_window = LeagueOfLegendsClientWindow()
     print(f"Pos: {lol_client_window.get_pos()}")
     print(f"Size: {lol_client_window.get_size()}")
+
+    print(f"In Queue: {lol_client_window.in_queue()}")
