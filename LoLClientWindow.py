69adb2741ecb21e6e2a20ef791e0faebe6c378c1
DustinScharf <d.scharf.2002@gmail.com>
2021-08-25 15:35:18 +0200

    Imported from win32gui {FindWindow, GetWindowRect} pip install win32

--- LoLClientWindow.py
+++ LoLClientWindow.py
@@ -1,3 +1,6 @@
+from win32gui import FindWindow, GetWindowRect
+
+
 class LoLClientWindow(object):
     def __init__(self, window_name: str = "League of Legends"):
         self.window_name = window_name
