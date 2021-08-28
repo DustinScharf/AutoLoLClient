8c6b96dd89240df468a222d9434b9a197daf58ac
DustinScharf <d.scharf.2002@gmail.com>
2021-08-28 22:52:40 +0200

    Added message loop if accepted (accept is still todo)

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -1,6 +1,14 @@
+import time
+
 from AutoLoLClient.LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
 
 if __name__ == '__main__':
     lol_client = LeagueOfLegendsClientWindow()
     while True:
-        print(lol_client.get_state())
+        state = lol_client.get_state()
+        print(state)
+        if state == "found_game":
+            # TODO accept
+            for i in range(100):
+                lol_client.send_message("mid")
+                time.sleep(0.03)
