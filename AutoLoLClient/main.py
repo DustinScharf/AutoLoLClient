a61f83d0f4c037eaff68e2065c4deeb292d382b4
DustinScharf <d.scharf.2002@gmail.com>
2021-08-28 22:54:36 +0200

    Added accepted var to end the checking for game loop if a game was accepted

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -4,11 +4,13 @@ from AutoLoLClient.LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindo
 
 if __name__ == '__main__':
     lol_client = LeagueOfLegendsClientWindow()
-    while True:
+    accepted = False
+    while not accepted:
         state = lol_client.get_state()
         print(state)
         if state == "found_game":
             # TODO accept
+            accepted = True
             for i in range(100):
                 lol_client.send_message("mid")
                 time.sleep(0.03)
