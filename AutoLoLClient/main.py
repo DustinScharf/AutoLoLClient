fa7f21aa0e3f2e6b87c1fecbcaced30bbb765a26
DustinScharf <d.scharf.2002@gmail.com>
2021-08-28 22:39:25 +0200

    Created main methode in main class

---
+++ AutoLoLClient/main.py
@@ -0,0 +1,5 @@
+from AutoLoLClient.LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
+
+if __name__ == '__main__':
+    lol_client = LeagueOfLegendsClientWindow()
+    print(lol_client.get_pos())
