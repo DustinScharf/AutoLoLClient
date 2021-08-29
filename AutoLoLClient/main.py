9d9e3fb1fb213b38db61767c612b50b52bcb4907
DustinScharf <d.scharf.2002@gmail.com>
2021-08-30 00:13:27 +0200

    Removed old info

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -58,7 +58,6 @@ if __name__ == '__main__':
     layout.addWidget(chat_message_box)
 
     layout.addWidget(QLabel("Enter champion to pick (full name)"))
-    layout.addWidget(QLabel("[YET ONLY CHAMPION SEARCH, BUT NOT PICK IMPLEMENTED]"))  # TODO
     champion_pick_box = QLineEdit()
     layout.addWidget(champion_pick_box)
 
