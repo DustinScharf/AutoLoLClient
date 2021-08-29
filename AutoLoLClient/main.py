dfed3b62c4bb92c09d9f14b3ccd871118529046e
DustinScharf <d.scharf.2002@gmail.com>
2021-08-29 02:25:53 +0200

    Changed repeat chat message number

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -9,8 +9,9 @@ if __name__ == '__main__':
         state = lol_client.get_state()
         print(state)
         if state == "found_game":
+
             lol_client.accept_game()
             accepted = True
-            for i in range(100):
+            for i in range(30):
                 lol_client.send_message("mid")
                 time.sleep(0.03)
