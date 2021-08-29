a9bb26c6eac689819c8b3879aa17f7666fc3fcc7
DustinScharf <d.scharf.2002@gmail.com>
2021-08-29 02:28:22 +0200

    Added basic gui to new main

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -1,8 +1,22 @@
 import time
 
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+
 from AutoLoLClient.LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
 
 if __name__ == '__main__':
+    app = QApplication([])
+    window = QWidget()
+    window.setWindowTitle("AutoLoLClient")
+    window.setMinimumWidth(400)
+    window.setMinimumWidth(300)
+
+    layout = QVBoxLayout()
+
+    window.setLayout(layout)
+    window.show()
+    app.exec()
+
     lol_client = LeagueOfLegendsClientWindow()
     accepted = False
     while not accepted:
