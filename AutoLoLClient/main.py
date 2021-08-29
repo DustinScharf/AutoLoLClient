e1f78bbb3b961c9f80531862ac4e5b50107c1e81
DustinScharf <d.scharf.2002@gmail.com>
2021-08-29 02:26:17 +0200

    Removed print statement

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
     accepted = False
     while not accepted:
         state = lol_client.get_state()
-        print(state)
         if state == "found_game":
 
             lol_client.accept_game()
