e8dd8b333b7d08294490677a114cbe92083ab93a
DustinScharf <d.scharf.2002@gmail.com>
2021-09-01 15:48:31 +0200

    Implemented search thread to let the gui controllable while searching

--- AutoLoLClient/main.py
+++ AutoLoLClient/main.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 import pyautogui
@@ -6,6 +7,85 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QMe
 from LeagueOfLegendsClientWindow import LeagueOfLegendsClientWindow
 from win32api import GetMonitorInfo, EnumDisplayMonitors
 from win32gui import MoveWindow
+
+searching = False
+
+
+def wait_thread(check_queue: bool):
+    global searching
+
+    if searching:
+        return
+
+    searching = True
+
+    chat_message_box.setReadOnly(True)
+    chat_message_box.setStyleSheet("background-color: lightgray;")
+    champion_pick_box.setReadOnly(True)
+    champion_pick_box.setStyleSheet("background-color: lightgray;")
+
+    search_game_button.setDisabled(True)
+    wait_for_game_button.setDisabled(True)
+
+    text_to_send = chat_message_box.text()
+    accepted = False
+    while not accepted and searching:
+        state = lol_client.get_state()
+        print("I wait...")
+        if state == "found_game":
+
+            lol_client.accept_game()
+            accepted = True
+            searching = False
+            for i in range(30):
+                lol_client.send_message(text_to_send)
+                time.sleep(0.03)
+
+            if lol_client.in_champion_selection():
+                champion_to_pick = champion_pick_box.text()
+                if champion_to_pick != "":
+                    lol_client.search_champion(champion_to_pick)
+                    time.sleep(1)
+                    lol_client.pick_first_champion()
+            else:
+                accepted = False
+                searching = True
+        elif check_queue and state != "in_queue":
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+
+            msg.setWindowTitle("AutoLoLClient")
+            msg.setText("Not in queue anymore, please press a start button again")
+
+            msg.setStandardButtons(QMessageBox.Ok)
+
+            msg.exec()
+
+            chat_message_box.setReadOnly(False)
+            chat_message_box.setStyleSheet("background-color: white;")
+            champion_pick_box.setReadOnly(False)
+            champion_pick_box.setStyleSheet("background-color: white;")
+
+            chat_message_box.setReadOnly(False)
+            chat_message_box.setStyleSheet("background-color: white;")
+            champion_pick_box.setReadOnly(False)
+            champion_pick_box.setStyleSheet("background-color: white;")
+
+            searching = False
+            return
+
+    chat_message_box.setReadOnly(False)
+    chat_message_box.setStyleSheet("background-color: white;")
+    champion_pick_box.setReadOnly(False)
+    champion_pick_box.setStyleSheet("background-color: white;")
+
+    search_game_button.setDisabled(False)
+    wait_for_game_button.setDisabled(False)
+
+
+def cancel():
+    global searching
+    searching = False
 
 
 def search_game():
