5f36c576a02263b1cc9d3d0cb068b57efce161e3
DustinScharf <d.scharf.2002@gmail.com>
2021-09-13 13:13:36 +0200

    Added another possible error cause

--- AutoLoLClient/AutoLoLClient.py
+++ AutoLoLClient/AutoLoLClient.py
@@ -45,6 +45,7 @@ def wait_thread(check_queue: bool):
                 time.sleep(0.03)
 
             # TODO check here for errors (only in wait for game mode? only if text marked?)
+            # The error only triggers in some cases, maybe if a game was not accepted by one player and then re-found
             if lol_client.in_champion_selection():
                 champion_to_pick = champion_pick_box.text()
                 if champion_to_pick != "":
