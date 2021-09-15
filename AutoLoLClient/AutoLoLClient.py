06f4dd7c01cef23aa2fc2ecc7266635204ccd995
DustinScharf <d.scharf.2002@gmail.com>
2021-09-15 03:43:35 +0200

    Buttons will now enable again automatically after search fail

--- AutoLoLClient/AutoLoLClient.py
+++ AutoLoLClient/AutoLoLClient.py
@@ -78,6 +78,9 @@ def wait_thread(check_queue: bool):
             champion_pick_box.setReadOnly(False)
             champion_pick_box.setStyleSheet("background-color: white;")
 
+            search_game_button.setDisabled(False)
+            wait_for_game_button.setDisabled(False)
+
             searching = False
             return
 
